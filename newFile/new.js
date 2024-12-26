d9d55af00350f6f4db93299e45ddb4b57aa09ebc
satty2802 <130182436+satty2802@users.noreply.github.com>
2024-12-26 13:13:57 +0000

    commit

--- newFile/new.js
+++ newFile/new.js
@@ -1,1 +1,17 @@
-console.log("My first code")
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split('');
+console.log(words[3]);
+// Expected output: "fox"
+console.log(words);
+
+
+const chars = str.split(' ');
+console.log(chars[8]);
+// Expected output: "k"
+console.log(chars);
+
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
