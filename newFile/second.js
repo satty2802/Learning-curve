d658246570fdabf200e8ea10010116ca545e5ebd
satty2802 <130182436+satty2802@users.noreply.github.com>
2024-12-26 20:30:10 +0000

    ending strings

---
+++ newFile/second.js
@@ -0,0 +1,35 @@
+ //Maths and Number
+// const balance = new Number (100.56);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+// const thousands = 10000000 ;
+// console.log(thousands.toLocaleString('en-IN'));
+
+// console. log (Math);
+// console. log (Math.abs(-6) ) ;
+// console. log (Math.round (4.6));
+// console. log (Math.ceil (4.2));
+// console. log (Math.floor(4.9));
+// console.log(Math.sqrt(25));
+
+
+// const min = 10
+// const max = 20
+// console.log (Math.floor(Math. random() * (max-min + 1)) + min)
+
+// const myDate = new Date(2024,0,5);
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+
+const myDate = new Date("01-28-2025");
+console.log(myDate.toLocaleString());
+
+let time =  Date.now()
+console.log(myDate.getTime());
+console.log(myDate.toLocaleString('default',{weekday: "narrow"}));
+
