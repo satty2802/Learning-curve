ad596239de8560e24c96b0c9f0fa307bd4525579
satty2802 <130182436+satty2802@users.noreply.github.com>
2024-12-26 16:56:23 +0000

    lec commited

---
+++ newFile/lec.js
@@ -0,0 +1,19 @@
+"user strict";//code treas as a newer version of javascript
+const accountId = 1255;//This keyword is use to assign values that cannot be changed 
+let accountName = "Satyam Dixit"//This is more convinient to use 
+var accountCity = "Benglauru"/*Prefer not to use var
+because of issue in block scope and functional scope*/
+accountPassword = 11631 // variable can be assigned without using any keyword but it is not the correct way
+//code redability should be high
+ let num = 2**5 ;
+ console.log(num);
+// number => 2 to power 53
+// bigint
+// string =>
+// boolean
+// true/ false
+// null => standalone value
+// undefined
+//symbol=>uniquess
+console.log(null>=0);
+
